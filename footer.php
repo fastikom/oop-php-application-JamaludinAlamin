f7769b01b044725d7991b4dcc2d4dc9d9dc56706
JamaludinAlamin <jamal.awk@gmail.com>
2017-05-02 09:35:52 +0700

    oop-php

---
+++ footer.php
@@ -0,0 +1,8 @@
+<div class="container">
+	<div class="alert alert-info">
+    <strong></strong> <a href=""></a>@JamaludinAlamin
+	</div>
+</div>
+<script src="bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
